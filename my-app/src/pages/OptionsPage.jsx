9b5247cb7200752f93f324f4fa3f3994c38baff5
Fredrick Mwangi <fredmallin49@gmail.com>
2026-05-10 12:12:26 +0300

    initialize my project

--- my-app/src/pages/OptionsPage.jsx
+++ my-app/src/pages/OptionsPage.jsx
@@ -6,7 +6,7 @@ function OptionsPage() {
 
   return (
     <div className="options">
-      {/* Back butt
+      {/* Back bu
       <button className="back-btn" onClick={() => navigate(-1)}>
         ⬅ Back
       </button>
