dd50db14b4f8024b4f1762c127dcb7ca7a7a61b6
Fredrick Mwangi <fredmallin49@gmail.com>
2026-05-14 22:12:27 +0300

    initialize my project

--- my-app/src/pages/GalleryPage.jsx
+++ my-app/src/pages/GalleryPage.jsx
@@ -25,7 +25,7 @@ function GalleryPage() {
       caption: "Late night talks",
     },
     {
-      image: "my-app/public/linda 7.jpeg",
+      image: "/linda 7.jpeg",
       caption: "Late night talks",
     },
     {
