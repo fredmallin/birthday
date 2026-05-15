a221c0b9e40758d861930c3262336388ea5fe5af
Fredrick Mwangi <fredmallin49@gmail.com>
2026-05-15 09:44:06 +0300

    initialize my project

--- my-app/src/pages/MessagePage.jsx
+++ my-app/src/pages/MessagePage.jsx
@@ -11,33 +11,35 @@ function MessagePage() {
         <h2> A Special Message For You</h2>
 
         <p>
-          Happy Birthday   
+           Happy Birthday Linda 🎉❤️
         </p>
 
         <p>
-          Today is not just a special day for you, but also a reminder of how
-          important you are to the people around you. we are really grateful to
-          have you in my life.
+          Today is all about celebrating you — our sister, our cousin, our biggest sister, our best friend, and honestly one of the greatest blessings in our lives. You have such a beautiful heart, and the love, care, and warmth you give to everyone around you never go unnoticed.
         </p>
 
         <p>
-          I hope this new chapter brings you peace, growth, happiness, and
-          endless opportunities. May you achieve everything you’ve been
-          working for and more than you ever dreamed of.
+         Thank you for always being there for us, for guiding us, supporting us, making us laugh, and loving us so genuinely. You are the kind of person who makes life feel lighter and happier just by being present. Home feels like home because of you.
         </p>
 
         <p>
-          Never forget how strong, kind, and amazing you are. Even on days
-          when things feel difficult, keep going — because great things are
-          waiting for you ahead.
+         We truly thank God for your life, because you are heaven-sent. Your strength, kindness, and soft heart inspire us more than you know. Even when life gets hard, you still manage to smile and show love to the people around you, and that is something so special about you.
         </p>
 
         <p>
-          Enjoy your day, celebrate yourself and know that you are truly
-          appreciated 
+          As you grow older, we pray that happiness never leaves your side, that peace fills your heart, and that every dream you carry finds its way to you. May this new year of your life bring you endless blessings, beautiful memories, and love in its purest form.
+        </p>
+        <p>
+          Please never forget how deeply loved and appreciated you are. We are so lucky to have you.
         </p>
 
-        <h3>With love from sisi ni sisi </h3>
+        <p>
+          Happy Birthday once again to the best big sister anyone could ever ask for 🥹❤️
+
+        </p>
+
+        <h3>With love,
+Sisi ni Sisi ❤️ </h3>
       </div>
     </div>
   );
